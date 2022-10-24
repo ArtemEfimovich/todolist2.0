8735216be31d3ca8d0f77fa4340719e7b236fc11
ArtemEfimovich <artemfender@gmail.com>
2022-10-24 22:31:34 +0300

    test driven development

---
+++ src/todolists-reducer.ts
@@ -0,0 +1,60 @@
+import {FilterValueType, TodolistsType} from "./App";
+import {v1} from "uuid";
+
+
+export type  RemoveTodoListActionType = {
+    type:'REMOVE-TODOLIST',
+    id:string
+}
+
+export type AddTodolistActionType = {
+    type:'ADD-TODOLIST',
+    title: string
+}
+
+export type ChangeTodolistTitleActionType = {
+    type:'CHANGE-TODOLIST-TITLE',
+    id:string,
+    title:string
+}
+
+export type ChangeTodolistFilterActionType= {
+    type:'CHANGE-TODOLIST-FILTER' ,
+    id:string,
+    filter:FilterValueType
+}
+
+
+type ActionsType = RemoveTodoListActionType|
+    AddTodolistActionType|
+    ChangeTodolistTitleActionType|
+    ChangeTodolistFilterActionType
+
+
+export const todolistsReducer = (state: TodolistsType[], action: ActionsType) => {
+    switch (action.type) {
+        case 'REMOVE-TODOLIST':
+            return state.filter(tl => tl.id !== action.id)
+        case 'ADD-TODOLIST':
+            let newTodolistId = v1()
+            let newTodolist: TodolistsType = {id: newTodolistId, title: action.title, filter: 'all'}
+            return [...state, newTodolist]
+        case 'CHANGE-TODOLIST-TITLE':
+            const todolist = state.find(tl => tl.id === action.id)
+            if (todolist) {
+                todolist.title = action.title
+            }
+            return [...state]
+        case 'CHANGE-TODOLIST-FILTER':
+            const todolistFilter = state.find(tl => tl.id === action.id)
+            if(todolistFilter){
+                todolistFilter.filter = action.filter
+            }
+            return [...state]
+        default:
+            throw new Error("I don`t understand this type ")
+    }
+}
+
+
+
