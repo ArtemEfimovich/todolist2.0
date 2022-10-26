3fa6b7518749fabcab188cbc401799618988b54c
ArtemEfimovich <artemfender@gmail.com>
2022-10-26 21:55:29 +0300

    test driven development, tasks reducer add task

--- src/tasks-reducer.ts
+++ src/tasks-reducer.ts
@@ -1,4 +1,5 @@
 import {FilterValueType, TasksStateType} from "./App";
+import {v1} from "uuid";
 
 
 
@@ -9,8 +10,9 @@ export type  RemoveTaskActionType = {
 }
 
 export type AddTaskActionType = {
-    type:'ADD-TODOLIST',
-    title: string
+    type:'ADD-TASK',
+    title: string,
+    todolistId:string
 }
 
 export type ChangeTaskTitleActionType = {
@@ -40,6 +42,11 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
              let newTask = todolistTask.filter(t => t.id !== action.taskId)
             stateCopy[action.todolistId] = newTask
             return stateCopy
+        case 'ADD-TASK':
+            let task = {id: v1(), title: action.title, isDone: false}
+            let todolistTasks = state[action.todolistId]
+            state[action.todolistId] = [task, ...todolistTasks]
+            return state
         default:
             throw new Error("I don`t understand this type ")
     }
@@ -49,3 +56,9 @@ export const tasksReducer = (state: TasksStateType, action: ActionsType) => {
 export const removeTaskAC = (taskId: string, todolistId: string): RemoveTaskActionType => {
     return {type: 'REMOVE-TASK', taskId, todolistId}
 }
+
+export const addTaskAC = (title:string,todolistId:string):AddTaskActionType=>{
+ return  {type:'ADD-TASK',title,todolistId}
+}
+
+
