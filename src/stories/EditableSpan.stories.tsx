88152b07db3c71aab9070884d0346b6b367cb1a0
ArtemEfimovich <artemfender@gmail.com>
2022-11-19 22:34:08 +0300

    added editable span stories

---
+++ src/stories/EditableSpan.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import {EditableSpan} from "../components/EditableSpan";
+import {action} from "@storybook/addon-actions";
+
+ export default {
+    title: 'TodoList/EditableSpan',
+    component: EditableSpan,
+    argsTypes: {
+        onClick:{
+            description: 'Button inside form clicked'
+        }
+    }
+} as ComponentMeta<typeof EditableSpan>;
+
+const Template: ComponentStory<typeof EditableSpan> = (args) => <EditableSpan {...args} />;
+
+export const EditableSpanExample = Template.bind({});
+EditableSpanExample.args = {
+    onChange:action('EditableSpan value changed')
+}
